ad506d1f5dfeb0d08cd2866486961d62a91ee5bd
Steven Paez <stevenpm131@gmail.com>
2026-03-29 21:19:46 -0500

    Actualizacion, mejoras

--- grafo.py
+++ grafo.py
@@ -1,15 +1,15 @@
-from collections import deque
 
+CALLE_100= 'Calle 100'
 
 grafo = {
     'Portal Americas': ['Banderas'], 
     'Banderas': ['Puente Aranda'],
     'Puente Aranda': ['Ricaurte'],
     'Ricaurte':['Av. Jimenez','Calle 75'],
-    'Calle 75': ['Calle 100'],
+    'Calle 75': [CALLE_100],
     'Av. Jimenez': ['Calle 39'],
     'Calle 39' : ['Flores'],
-    'Flores': ['Calle 100'],
-    'Calle 100' : ['Mazuren'],
+    'Flores': [CALLE_100],
+    CALLE_100 : ['Mazuren'],
     'Mazuren': ['Portal Norte']
 }
--- grafo_dijkstra.py
+++ grafo_dijkstra.py
@@ -1,12 +1,13 @@
 
+CALLE_100 = 'Calle 100'
 grafo_dijkstra = {
     'Portal Americas':[('Banderas', 1)],
     'Banderas':[('Puente Aranda',1)],
     'Puente Aranda':[('Ricaurte',1)],
-    'Ricaurte':[('Av. Jimenez',1),('Calle 100',6)],
+    'Ricaurte':[('Av. Jimenez',1),(CALLE_100,6)],
     'Av. Jimenez':[('Calle 39',1)],
     'Calle 39':[('Flores',1)],
-    'Flores':[('Calle 100',1)],
-    'Calle 100':[('Mazuren',1)],
+    'Flores':[(CALLE_100,1)],
+    CALLE_100:[('Mazuren',1)],
     'Mazuren':[('Portal Norte',1)]
 }
